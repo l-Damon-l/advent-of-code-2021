55be76ce2caf904076a8c96d49cf47b4ddc8c880
Dæmon <xdamon@hotmail.co.uk>
2021-12-02 17:12:38 +0000

    Tidied up a bit

--- C++/day1.cpp
+++ C++/day1.cpp
@@ -34,7 +34,7 @@ int main() {
 
     // get lines from txt file
     vector<int> depths;
-    for (std::string line; std::getline(std::cin, line);) {
+    for (string line; getline(std::cin, line);) {
         depths.push_back(stoi(line));
     }
     
